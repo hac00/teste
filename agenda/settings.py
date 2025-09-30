ab9b4db3c284a71983718b869891102184bb7d29
= <=>
2025-09-30 15:05:23 -0300

    email & agendamento_exibir copy miguelcalisto

--- agenda/settings.py
+++ agenda/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_URL
+from django.conf.global_settings import MEDIA_URL, EMAIL_BACKEND
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +142,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'henriqueac00@gmail.com'
+EMAIL_HOST_PASSWORD = 'hyzutwknnscloujw'
+DEFAULT_FROM_EMAIL = 'lavacar'
--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
 from django.core.paginator import Paginator
+from django.template.loader import render_to_string
 # from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -125,5 +127,29 @@ class AgendamentoExibir(DetailView):
                                 produto.save()
                 agendamento.status = 'F'
                 agendamento.save()
+                self.enviar_email(agendamento)
         return agendamento
 
+    def enviar_email(self, agendamento):
+        email = []
+        email.append(agendamento.cliente.email)
+        descricao = []
+        for servico in agendamento.servicos:
+            descricao.append(f'{servico} - R$ {servico.preco} ({servico.get_situacao_display()})')
+
+        dados = {'cliente': agendamento.cliente.nome,
+                 'horario': agendamento.horario,
+                 'funcionario': agendamento.funcionario.nome,
+                 'descricao': descricao,
+                 'valor': agendamento.valor, }
+
+        texto_email = render_to_string('emails/texto_email.txt', dados)
+        html_email = render_to_string('emails/texto_email.html', dados)
+        send_mail(subject='LavaCar - Serviço concluído',
+                  message=texto_email,
+                  from_email='henriqueac00@gmail.com',
+                  recipient_list=email,
+                  html_message=html_email,
+                  fail_silently=False,
+                  )
+        return redirect('agendamentos')
