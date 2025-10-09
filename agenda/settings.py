f6fe7cb66af35ac91d4d8ee30433654563cb8469
= <=>
2025-10-09 14:30:48 -0300

    add localhost to settings

--- agenda/settings.py
+++ agenda/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config("DEBUG", cast=bool)
 
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', 'localhost').split(',')
 
 
 # Application definition
